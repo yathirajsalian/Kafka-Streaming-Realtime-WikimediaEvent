b612aa5dff21f437928db60212d266b61c313d6e
yathirajsalian <yathiraj78@gmail.com>
2025-06-15 20:00:25 +0530

    Changes to store the consumed data in db

--- Kafka-Streaming-Realtime-Project/kafka-consumer-database/src/main/java/com/ygn/kafkaConsumer/kafkaDatabaseConsumer.java
+++ Kafka-Streaming-Realtime-Project/kafka-consumer-database/src/main/java/com/ygn/kafkaConsumer/kafkaDatabaseConsumer.java
@@ -1,13 +1,19 @@
 package com.ygn.kafkaConsumer;
 
+import com.ygn.kafkaConsumer.Repository.WikimediaDataRepository;
+import com.ygn.kafkaConsumer.entity.WikimediaData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class kafkaDatabaseConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(kafkaDatabaseConsumer.class);
+
+    @Autowired
+    private WikimediaDataRepository dataRepository;
 
     @KafkaListener(
                 topics = "wikimedia_recentchange",
@@ -16,5 +22,8 @@ public class kafkaDatabaseConsumer {
     public void cosnume(String eventMessage)
     {
         LOGGER.info(String.format("Message recieved -> %s",eventMessage));
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+        dataRepository.save(wikimediaData);
     }
 }
